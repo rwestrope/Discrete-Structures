882af25bde138b9b4420745e051158294ccdfffc
rwestrope <rewestrope@gmail.com>
2023-01-31 13:45:16 -0600

    Total cost function working

--- Chapter_3.py
+++ Chapter_3.py
@@ -2,25 +2,25 @@
 
 BeverageMenu = {
     "Water: $": "1.00",
-    "Dr. Pepper": "$2.00",
-    "Coca-Cola": "$2.00",
+    "Dr. Pepper: $": "2.00",
+    "Coca-Cola: $": "2.00",
     }
 BurgerMenu = {
-    "Classic Burger": "$7.00",
-    "Organic Sirloin Burger": "$9.00",
-    "Greek Burger": "$10.00",
-    "Bacon Cheeseburger":"$8.00", 
-    "Quinoa and Black Bean Burger":"$9.00"
+    "Classic Burger: $": "7.00",
+    "Organic Sirloin Burger: $": "9.00",
+    "Greek Burger: $": "10.00",
+    "Bacon Cheeseburger: $":"8.00", 
+    "Quinoa and Black Bean Burger: $":"9.00"
     }
 SideMenu = {
-    "Onion Rings": "$3.50",
-    "Fries": "$3.00", 
-    "Sweet Potato Fries": "$4.00", 
-    "Mozzarella Sticks": "$3.00"
+    "Onion Rings: $": "3.50",
+    "Fries: $": "3.00", 
+    "Sweet Potato Fries: $": "4.00", 
+    "Mozzarella Sticks: $": "3.00"
     }
 DessertMenu = {
-    "Cookie": "$2.50", 
-    "Brownie": "$2.50"
+    "Cookie: $": "2.50", 
+    "Brownie: $": "2.50"
     }
 
 def GetBeverages():
@@ -28,7 +28,7 @@ def GetBeverages():
     print("Beverages:")
     for beverage, price in BeverageMenu.items():
         print(beverage, price)
-    user_beverage = input("Choose a beverage")
+    user_beverage = input("Choose a beverage\n:")
     return user_beverage
 
 def GetBurgers():
@@ -36,7 +36,7 @@ def GetBurgers():
     print("\nBurgers")
     for burger, price in BurgerMenu.items():
         print(burger, price)
-    user_burger = input("Choose a beverage")
+    user_burger = input("Choose a beverage\n:")
     return user_burger
 
 def GetSides():
@@ -44,7 +44,7 @@ def GetSides():
     print("\nSides")
     for side, price in SideMenu.items():
         print(side, price)
-    user_side = input("Choose a beverage")
+    user_side = input("Choose a beverage\n:")
     return user_side
 
 def GetDesserts():
@@ -52,15 +52,40 @@ def GetDesserts():
     print("\nDesserts")
     for dessert, price in DessertMenu.items():
         print(dessert, price)
-    user_dessert = input("Choose a beverage")
+    user_dessert = input("Choose a beverage\n:")
     return user_dessert
 
 def GetUserTotal():
+    global user_dessert, user_beverage, user_burger, user_side, total_cost
+    #global total_cost, beverage_cost, burger_cost, side_cost, dessert_cost
+    beverage_cost = 0
+    burger_cost = 0
+    side_cost = 0
+    dessert_cost = 0
     for beverage, cost in BeverageMenu.items():
-        if beverage == user_beverage:
-            print(cost)
-            beverage_cost = int(cost)
-            return beverage_cost
+        if beverage.replace(": $", "") == user_beverage:
+            
+            print(f"The cost of your beverage is ${cost}")
+            beverage_cost += float(cost)
+            
+    for burger, cost in BurgerMenu.items():
+        if burger.replace(": $", "") == user_burger:
+            print(f"The cost of your burger is ${cost}")
+            burger_cost += float(cost)
+            
+    for side, cost in SideMenu.items():
+        if side.replace(": $", "") == user_side:
+            print(f"The cost of your side is ${cost}")
+            side_cost += float(cost)
+            
+    for dessert, cost in DessertMenu.items():
+        if dessert.replace(": $", "") == user_dessert:
+            print(f"The cost of your dessert is ${cost}")
+            dessert_cost += float(cost)
+            
+    total_cost = beverage_cost + burger_cost + side_cost + dessert_cost
+    print(f"Your total cost is ${total_cost}")
+    
 
 
 
