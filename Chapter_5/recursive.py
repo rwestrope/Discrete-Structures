8e02ec13f7553fd8b1e36bd288ca0b9e19d00cd6
rwestrope <rewestrope@gmail.com>
2023-02-16 12:07:18 -0600

    counter for recursive

--- Chapter_5/recursive.py
+++ Chapter_5/recursive.py
@@ -1,10 +1,36 @@
-def fibonacci(n):
-    if n <= 1:
-        return n
-    else:
-        return fibonacci(n-1) + fibonacci(n-2)
 
+class OperationCounter:
+    def __init__(self):
+        self.function_call_count = 0
+        self.subtract_1_count = 0
+        self.subtract_2_count = 0
+
+    def GetFunctionCount(self):
+        return self.function_call_count
+
+    def GetSubtract1Count(self):
+        return self.subtract_1_count
+
+    def GetSubtract2Count(self):
+        return self.subtract_2_count
+
+
+
+operation_counter = OperationCounter()
+
+def fibonacci(num):
+    operation_counter.function_call_count += 1
+    if num <= 1:
+        return num
+    else:
+        operation_counter.subtract_1_count += 1
+        operation_counter.subtract_2_count += 1
+        return fibonacci(num-1) + fibonacci(num-2)
 
 
 
 print(fibonacci(10))
+
+print(f"you called fibonacci {operation_counter.GetFunctionCount()} times")
+print(f"you subtracted 1 {operation_counter.GetSubtract1Count()} times")
+print(f"you subtracted 2 {operation_counter.GetSubtract2Count()} times")
