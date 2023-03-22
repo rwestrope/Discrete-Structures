eb8a0600a18c15cd4fc0de4ccb1cd51f4877f633
rwestrope <rewestrope@gmail.com>
2023-03-22 15:13:37 -0500

    more work on chapter 8 homework

--- Chapter_8.py
+++ Chapter_8.py
@@ -1,44 +1,58 @@
+from math import sqrt
 '''
 use fibonacci to make a list of fibonacci numbers (for loop would be nice)
 use sieve to check which of those numbers is prime
 '''
 
-
-def fibonacci_iterative(num):
-    if num == 0:
-        return 0
-    elif num == 1:
-        return 1
+def fibonacci(num):
+    sequence = [0, 1]
+    if num <= 0:
+        return [0]   
     else:
         # Initialize variables to store the first two numbers in the sequence
         a, b = 0, 1
         # Loop through the sequence up to num, updating a and b at each step
         for index in range(2, num+1):
-            #operation_counter.function_call_count += 1
             c = a + b
+            sequence.append(c)
             a = b
             b = c
-        return b
+        #return b
+        return sequence
     
 
-def sieve_of_eratosthenes(n):
-    # Create a boolean array "prime[0..n]" and
-    # initialize all entries it as true.
-    prime = [True for i in range(n+1)]
-    p = 2
-    while(p * p <= n):
-         
-        # If prime[p] is not changed, then it is a prime
-        if (prime[p] == True):
-             
-            # Update all multiples of p
-            for i in range(p * p, n+1, p):
-                prime[i] = False
-        p += 1
+
+def prime_checker(checklist, primelist):
+    for num in checklist:
+        if num == 1:
+            pass
+        elif num > 1:
+            # check for factors
+            for i in range(2, round(sqrt(num))):
+               if (num % i) == 0:
+                   break
+            else:
+                primelist.append(num)
+       
+        # if input number is less than
+        # or equal to 1, it is not prime
+        else:
+            pass
     
-    # Count the number of prime numbers
-    count = 0
-    for p in range(2, n):
-        if prime[p]:
-            count += 1
-    return count
+    return primelist
+
+
+if __name__ == "__main__":
+
+    prime_fibonaccis = []
+
+    fib_nums = int(input("How many fibonacci numbers do you want to generate?\n:"))
+    fibonacci_sequence = fibonacci(fib_nums)
+    #print(fibonacci_sequence)
+
+    prime_checker(fibonacci_sequence, prime_fibonaccis)
+
+    print(f"Out of {fib_nums} fibonacci numbers, {len(prime_fibonaccis)} were prime.")
+    print("The prime fibonacci numbers were:")
+    for number in prime_fibonaccis:
+        print(number)
